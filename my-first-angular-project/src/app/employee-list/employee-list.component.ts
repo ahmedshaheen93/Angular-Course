696c330bf7791e774dcd33c36e609e755615a5e0
ahmed shaheen <ahmedshaheen93@yahoo.com>
2020-12-21 00:30:29 +0200

    create list of employees and view over employee-list component

--- my-first-angular-project/src/app/employee-list/employee-list.component.ts
+++ my-first-angular-project/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  employees :Employee[]= [
+    new Employee("ahmed","shaheen","ahmedshaheen676@yahoo.com",1000),
+    new Employee("amr","elbaz","amrelbaz12345@gmail.com",1000),
+    new Employee("mohamed","elsayed","mohamedelsayed@gmail.com",1000)
+  ];
   constructor() { }
 
   ngOnInit(): void {
